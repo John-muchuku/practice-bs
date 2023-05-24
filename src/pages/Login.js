a19df8689771098fa5626a76841de4ef0bc7fc69
John <john.muchuku@student.moringaschool.com>
2023-05-24 11:50:24 +0300

    done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,30 +2,35 @@ import React from 'react'
 
 function Login() {
   return (
-    <div className='container  mx-auto row'> 
-    
-<div className='col-md-4'></div>
-<div className='col-md-4 bg-light mt-4'>
-
-<form className=' '>
-<h1 >LOGIN</h1>
-  <div class="mb-3 ">
+    <div className='p-5 d-flex flex-column align-items-center'> <h1 className='text-dark'>
+<img width="37" height="37" src="https://img.icons8.com/material-outlined/24/login-rounded-right.png" alt="login-rounded-right"/>  𝙻𝙾𝙶𝙸𝙽 
+  
+</h1>
+<form>
+  <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
+    <label for="exampleInputPassword1" class="form-label ">Password</label>
+    <input type="password" class="form-control " id="exampleInputPassword1"/>
   </div>
   <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <input type="checkbox" class="form-check-input " id="exampleCheck1"/>
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  
+  <button type="submit" class="btn btn-dark">Submit</button>
+
+  <div className='form-text p-2'>Don't have an account? 
+    <a href="/Signup"> Signup</a>
+  </div>
+  
 </form>
-</div>
-<div className='col-md-4'></div>
+
+
+
     </div>
   )
 }
